da98542b0d48ece70a6630903cbc84719b544831
nrjshka <nrjshka@gmail.com>
2019-07-23 22:27:34 +0300

    Added: normal car view; Fixed: problem with styled canvas

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 "use strict";
 
-document.addEventListener('DOMContentLoaded', () => {
-  const canvas = document.querySelector('#canvas');
-  const context = canvas.getContext('2d');
+const canvas = document.querySelector('#canvas');
+const context = canvas.getContext('2d');
 
-  context.fillStyle = 'red';
-  context.fillRect(50, 50, 20, 20);
-});
+canvas.width = window.innerWidth; canvas.height = window.innerHeight;
+
+const getCarView = () => {
+  let carImage = new Image();
+  carImage.src = 'img/car.png';
+  carImage.onload = () => {
+    context.drawImage(carImage, 0, 0);
+  }
+};
+
+
+getCarView();
