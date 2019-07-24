21f532213bba1cb6d71017f62fcc6299193b4df5
nrjshka <nrjshka@gmail.com>
2019-07-24 12:59:21 +0300

    Removed: test cases

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 "use strict";
 
+let x = 0, y = 0, isMoving = false;
 const canvas = document.querySelector('#canvas');
 const context = canvas.getContext('2d');
 
+const carImage = new Image();
+carImage.src = 'img/car.png';
+
 canvas.width = window.innerWidth; canvas.height = window.innerHeight;
 
-const getCarView = () => {
-  let carImage = new Image();
-  carImage.src = 'img/car.png';
-  carImage.onload = () => {
-    context.drawImage(carImage, 0, 0);
-  }
-};
-
-
-getCarView();
+context.drawImage(carImage, 0, 0);
